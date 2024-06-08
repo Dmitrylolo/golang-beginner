0d7a0fc5c7e817071db3dbdfbf6dc61e72481b20
Dmitry Chernenkov <dmitrychee@gmail.com>
2024-06-08 04:32:31 +0300

    variables

---
+++ variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	// var welcome string = "Hello, World!"
+	const welcome string = "Hello, World!"
+	// welcome := "Hello, World!"
+	println(welcome)
+
+	//myAge := 38
+	//myAge += 22
+
+	// var myAge int = 22
+	// println(myAge)
+
+	// const pi float32 = 3.14
+	const pi = 3.14
+	var size float64 = 1.0
+
+	println(size * pi)
+
+}
